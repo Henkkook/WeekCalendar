cca014eccd7b8b0beb6f4233bb2e3d5c334cfbce
D <K>
2023-02-24 15:21:58 +0500

    Initial Commit

---
+++ WeekCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekCalendar
+//
+//  Created by Denis Kalugin on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
